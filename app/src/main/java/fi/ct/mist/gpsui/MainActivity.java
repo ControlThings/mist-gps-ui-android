5d17c42f778400c0d43aa05f866e969db14c1506
André Kaustell <andre.kaustell@controlthings.fi>
2017-01-18 15:21:15 +0200

    Cleaned up the example code a little bit.

--- app/src/main/java/fi/ct/mist/gpsui/MainActivity.java
+++ app/src/main/java/fi/ct/mist/gpsui/MainActivity.java
@@ -1,33 +1,24 @@
 package fi.ct.mist.gpsui;
 
 import android.content.Intent;
-import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.bson.BsonBinaryWriter;
-import org.bson.BsonWriter;
-import org.bson.io.BasicOutputBuffer;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 import mist.MistService;
 import mist.Peer;
-import mist.RequestInterface;
 import mist.api.Control;
 import mist.api.Mist;
-import mist.sandbox.Callback;
 
 public class MainActivity extends AppCompatActivity {
 
-    private boolean mBound = false;
-    private MistService mistService;
     private TextView peerOnlineState;
     private Switch enabled;
     private TextView counter;
@@ -43,23 +34,22 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MistService.class);
         startService(intent);
 
+        // Login to sandbox
         Mist.login(new Mist.LoginCb() {
             @Override
             public void cb(boolean connected) {
+                // on successful login to sandbox
                 ready();
             }
 
             @Override
-            public void err(int code, String msg) {
-
-            }
+            public void err(int code, String msg) {}
 
             @Override
-            public void end() {
-
-            }
+            public void end() {}
         });
 
+        // Get handles to ui components
         peerOnlineState = (TextView) findViewById(R.id.peerOnlineState);
         enabled = (Switch) findViewById(R.id.enabled);
         counter = (TextView) findViewById(R.id.counter);
@@ -71,6 +61,10 @@ public class MainActivity extends AppCompatActivity {
     int id = 0;
 
     private void follow(final Peer peer) {
+        if (id != 0) {
+            Control.cancel(id);
+            id = 0;
+        }
         id = Control.follow(peer, new Control.FollowCb() {
             @Override
             public void cbBool(String epid, boolean value) {
@@ -83,8 +77,6 @@ public class MainActivity extends AppCompatActivity {
             public void cbInt(final String epid, final int value) {
 
                 String str = Integer.toString(value);
-
-                Log.d("Follow", epid + " : " + value);
 
                 if (epid.equals("counter")) {
                     counter.setText(str);
@@ -111,19 +103,13 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void cbString(String epid, String value) {
-
-            }
+            public void cbString(String epid, String value) {}
 
             @Override
-            public void err(int code, String msg) {
-
-            }
+            public void err(int code, String msg) {}
 
             @Override
-            public void end() {
-
-            }
+            public void end() {}
         });
 
         enabled.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -132,168 +118,102 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Writing enabled to "+b, Toast.LENGTH_SHORT).show();
                 Control.write(peer, "enabled", b, new Control.WriteCb() {
                     @Override
-                    public void cb() {
-
-                    }
+                    public void cb() {}
 
                     @Override
-                    public void err(int code, String msg) {
-
-                    }
+                    public void err(int code, String msg) {}
 
                     @Override
-                    public void end() {
-
-                    }
+                    public void end() {}
                 });
             }
         });
 
     }
 
-    private void cancel() {
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        if (id != 0) {
-                            Control.cancel(id);
-                        }
+    private void updatePeersList(ArrayList<Peer> peers) {
+
+        if(peers.size() > 1) {
+            // This demo app does not support multiple peers
+            Toast.makeText(getApplicationContext(), "Warning: Multiple devices not supported in this app.", Toast.LENGTH_LONG).show();
+        }
+
+        for (final Peer peer : peers) {
+            if(peer.isOnline()) {
+                Toast.makeText(getApplicationContext(), "Peer is online.", Toast.LENGTH_SHORT).show();
+                peerOnlineState.setText("Peer is online.");
+                Control.model(peer, new Control.ModelCb() {
+                    @Override
+                    public void cb(JSONObject data) {
+                        follow(peer);
                     }
-                },
-                10000);
+
+                    @Override
+                    public void err(int code, String msg) {}
+
+                    @Override
+                    public void end() {}
+                });
+            } else {
+                Toast.makeText(getApplicationContext(), "Peer is offline.", Toast.LENGTH_SHORT).show();
+                peerOnlineState.setText("Peer is offline.");
+            }
+        }
     }
 
     private void ready() {
 
-
-
         Mist.signals(new Mist.SignalsCb() {
             @Override
             public void cb(String signal) {
-                Log.d("Signals", signal);
-                if (signal.equals("peers")) {
+                if (signal.equals("peers") || signal.equals("ok")) {
                     Mist.listPeers(new Mist.ListPeersCb() {
                         @Override
                         public void cb(ArrayList<Peer> peers) {
-                            for (final Peer peer : peers) {
-                                if(peer.isOnline()) {
-                                    Toast.makeText(getApplicationContext(), "Peer is online.", Toast.LENGTH_SHORT).show();
-                                    peerOnlineState.setText("Peer is online.");
-                                    Control.model(peer, new Control.ModelCb() {
-                                        @Override
-                                        public void cb(JSONObject data) {
-                                            Log.d("Model:", data.toString());
-                                            follow(peer);
+                            if (peers.size() == 0) {
+                                // if there are no peers, request settings page for this app to be shown
+                                // this feature will enable claiming wifi devices etc.
+                                Mist.settings(new Mist.SettingsCb() {
+                                    @Override
+                                    public void cb() {}
 
-                                        }
+                                    @Override
+                                    public void err(int i, String s) {}
 
-                                        @Override
-                                        public void err(int code, String msg) {
-
-                                        }
-
-                                        @Override
-                                        public void end() {
-
-                                        }
-                                    });
-                                } else {
-                                    Toast.makeText(getApplicationContext(), "Peer is offline.", Toast.LENGTH_SHORT).show();
-                                    peerOnlineState.setText("Peer is offline.");
-                                }
+                                    @Override
+                                    public void end() {}
+                                });
                             }
+
+                            updatePeersList(peers);
                         }
 
                         @Override
-                        public void err(int code, String msg) {
-
-                        }
+                        public void err(int code, String msg) {}
 
                         @Override
-                        public void end() {
-
-                        }
+                        public void end() {}
                     });
                 }
-
             }
 
             @Override
-            public void err(int code, String msg) {
-
-            }
+            public void err(int code, String msg) {}
 
             @Override
-            public void end() {
-
-            }
+            public void end() {}
 
         });
-
-
-        Mist.listPeers(new Mist.ListPeersCb() {
-            @Override
-            public void cb(ArrayList<Peer> peers) {
-                if (peers.size() == 0) {
-                    Mist.settings(new Mist.SettingsCb() {
-                        @Override
-                        public void cb() {
-
-                        }
-
-                        @Override
-                        public void err(int i, String s) {
-
-                        }
-
-                        @Override
-                        public void end() {
-
-                        }
-                    });
-                }
-
-                for (final Peer peer : peers) {
-                    Control.model(peer, new Control.ModelCb() {
-                        @Override
-                        public void cb(JSONObject data) {
-                            Log.d("Model:", data.toString());
-
-                            counter.setText("This is bahamas.");
-
-                            follow(peer);
-                        }
-
-                        @Override
-                        public void err(int code, String msg) {
-
-                        }
-
-                        @Override
-                        public void end() {
-
-                        }
-                    });
-                }
-            }
-
-            @Override
-            public void err(int code, String msg) {
-
-            }
-
-            @Override
-            public void end() {
-
-            }
-        });
-
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        cancel();
+
+        if (id != 0) {
+            // Cancel the last follow request
+            Control.cancel(id);
+        }
     }
 }
 
